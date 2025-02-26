2a8054211e6b62f5f6a523f3b19fd11ee247f773
Ayush Tiwari <ayush.tiwari@parkhya.net>
2025-02-26 19:05:07 +0530

    Auth

---
+++ parkhyaconnect/parkhyaconnect/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="130350895686-alvovl0rqcao7j767pcdmp2k67nmgnrc.apps.googleusercontent.com">
+    
+    <App />
+    </GoogleOAuthProvider>;
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
